7cf84225a8347ecb6adfba15b2bae458383bda26
Ruby Chen <mp4bj6ruby@gmail.com>
2016-11-06 20:02:28 +0800

    會員資料修改, 密碼修改

---
+++ src/app/component/widgets/ProfileForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <form v-if="formData" :id="formData.id" :name="formData.name">
+    <h1>{{formData.title}}</h1>
+    <fieldset style="border:0;">
+      <div v-for="(active,labelKey) in formData.display" v-if="active" class="form-group">
+        <input type="text" class="form-control input-lg" :placeholder="formData.placeholder[labelKey]" v-model="formData.value[labelKey]">
+      </div>
+      <div class="span5"></div>
+      <div class="form-group">
+        <div class="row">
+          <div class="col-xs-12 col-md-12"><a href="#" class="btn btn-login btn-block btn-lg" @click.prevent="formData.onSubmit(formData.value)">登 入</a></div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="row">
+          <div class="col-xs-12 col-md-6"><a href="#" class="btn btn-is btn-block" @click.prevent="formData.onForget">忘記密碼</a></div>
+          <div class="col-xs-12 col-md-6"><a href="#" class="btn btn-is btn-block" @click.prevent="formData.onJoin">還沒有帳號?</a></div>
+        </div>
+      </div>
+    </fieldset>
+  </form>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        msg:'hello vue'
+      }
+    },
+    props: {
+      'formData': {
+        type: Object,
+        default: null
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+
+</style>
---
+++ src/app/component/widgets/PwForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <form v-if="formData" class=" col-md-offset-2 col-md-8">
+    <h1>{{formData.title}}</h1>
+    <div class="form-group row" v-for="(active,labelKey) in formData.display" v-if="active">
+      <label class="col-md-3 col-xs-12 col-form-label">{{formData.label[labelKey]}}</label>
+      <div class="col-md-9 col-xs-12">
+        <input class="form-control" type="text"
+               v-model="formData.value[labelKey]"
+               :disabled="!modifyMode">
+      </div>
+    </div>
+
+    <div class="row">
+      <div v-if="modifyMode" class="col-xs-12 col-md-6">
+        <a href="#" class="btn btn-is btn-block btn-lg" @click.prevent="formData.onSubmit(formData.value)">確定修改</a>
+      </div>
+      <div v-else class="col-xs-12 col-md-6">
+        <a href="#" class="btn btn-is btn-block btn-lg" @click.prevent="formData.onModify">修改會員資料</a>
+      </div>
+      <div class="col-xs-12 col-md-6"><a href="#" class="btn btn-is btn-block btn-lg" @click.prevent="formData.onModifyPw">修改密碼</a></div>
+    </div>
+  </form>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        msg:'hello vue'
+      }
+    },
+    props: {
+      'formData': {
+        type: Object,
+        default: null
+      },
+      'modifyMode': {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+
+</style>
